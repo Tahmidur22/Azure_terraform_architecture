e62739543d5170c2713543c063864a5ad6466660
BigTamz <bigtamz@bigtamzs-mbp.home>
2023-08-15 13:29:30 +0100

    network bastion configuration

--- modules /networking/main.tf
+++ modules /networking/main.tf
@@ -25,4 +25,36 @@ resource "azurerm_subnet" "subnets" {
     ]
 }
 
+resource "azurerm_subnet" "bastionsubnet" {
+    name = "AzureBastionSubnet"
+    resource_group_name  = var.resource_group_name
+    virtual_network_name = var.virtual_network_name
+    address_prefixes     = ["10.0.10.0/24"]
+    depends_on = [ 
+        azurerm_virtual_network.network 
+    ]
+}
+
+resource "azurerm_public_ip" "bastionip" {
+    name = "bastion-ip"
+    resource_group_name  = var.resource_group_name
+    location = var.location
+    allocation_method = "Static"
+    sku = "Standard"
+    depends_on = [ 
+        module.general_module.resource_group
+    ]
+}
+
+resource "azurerm_bastion_host" "appbastion" {
+    name = "appbastion"
+    resource_group_name  = var.resource_group_name
+    location = var.location
+
+    ip_configuration {
+      name = "configuration"
+      subnet_id = azurerm_subnet.bastionsubnet.id
+      public_ip_address_id = azurerm_public_ip.bastionip.id
+    }
+}
 
