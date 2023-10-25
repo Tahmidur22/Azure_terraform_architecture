b18415b4a9ec7b2e42134330a5b5cf02c7a54b90
tahmidur22 <tahmidur55@gmail.com>
2023-10-25 15:51:40 +0100

    storage account integration

---
+++ modules /storage/storageaccount.tf
@@ -0,0 +1,28 @@
+resource "azurerm_storage_account" "storageaccount" {
+  name                     = var.storage_account_name
+  resource_group_name      = var.resource_group_name
+  location                 = var.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  account_kind             = "StorageV2"
+}
+
+resource "azurerm_storage_container" "container" {
+  name                  = var.container_name
+  storage_account_name  = var.storage_account_name
+  container_access_type = var.container_access
+  depends_on = [ 
+    azurerm_storage_account.storageaccount 
+    ]
+}
+
+resource "azurerm_storage_blob" "example" {
+  name                   = "main.tf"
+  storage_account_name   = var.storage_account_name
+  storage_container_name = var.container_name
+  type                   = "Block"
+  source                 = "main.tf"
+  depends_on = [
+    azurerm_storage_container.container 
+    ]
+}
